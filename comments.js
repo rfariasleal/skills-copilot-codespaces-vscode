77d97246099507a98214ec9bbac41453630b7c80
rfariasleal <rodrigo.farias.leal@avanade.com>
2025-02-06 19:04:12 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,8 @@
+// Create web server with Node.js
+// https://www.youtube.com/watch?v=G8w2z2jB0Q4&list=PLillGF-RfqbZ7P6v9l2c6u7dDgI0x9YbS&index=1
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const url = require('url');
+const { parse } = require('querystring');
