b4e091d9bf9831b28b42f0482c37375a46d208d8
rfariasleal <rodrigo.farias.leal@avanade.com>
2025-02-06 18:58:47 +0000

    Copilot third commit

--- .github/comments.js
+++ .github/comments.js
@@ -1,2 +1,14 @@
-// Create web server to listen on port 3000
-// This server will
+// Create web server to receive GitHub webhook events
+const { Octokit } = require("@octokit/rest");
+const { request } = require("@octokit/request");
+const { createNodeMiddleware, createProbot } = require("probot");
+const { Probot } = require("probot");
+const { EOL } = require("os");
+const { readFileSync, writeFileSync } = require("fs");
+const {
+  addLabels,
+  getIssueNumber,
+  getPrTitle,
+  getPullRequest,
+  getRepoInfo,
+} = require("./utils");
